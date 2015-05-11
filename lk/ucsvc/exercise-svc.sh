99ef3ccf869400e234155947cc4737cf57b87538
pmcgrath <pmcgrath@gmail.com>
2015-05-11 21:59:02 +0100

    Updated to cater for basic authentication

--- lk/ucsvc/exercise-svc.sh
+++ lk/ucsvc/exercise-svc.sh
@@ -2,12 +2,15 @@
 
 # This was just for starting, should now use the ucsvc_client go app as trying to parse http headers etc with curl is too messy 
 svc_url=http://localhost:8090
+user_name=ted
+password=toe
+credentials=$user_name:$password
 
 echo '***** GET ALL USERS'
-curl -v -X GET "$svc_url/users" -H 'Accept: application/json'
+curl -v -u $credentials -X GET "$svc_url/users" -H 'Accept: application/json'
 
 echo '***** CREATING USER'
-curl -v -X POST "$svc_url/users" -H 'Content-Type: application/json' -H 'Accept: application/json' -d '{"name": "ted toe"}'
+curl -v -u $credentials -X POST "$svc_url/users" -H 'Content-Type: application/json' -H 'Accept: application/json' -d '{"name": "larry murphy"}'
 
 echo '***** GET ALL USERS'
-curl -v -X GET "$svc_url/users" -H 'Accept: application/json'
+curl -v -u $credentials -X GET "$svc_url/users" -H 'Accept: application/json'
